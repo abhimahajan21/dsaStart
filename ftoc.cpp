a37d3ab7bdcdbb7d73dfd749866cf2286e0fd6db
abhimahajan21 <abhi21mahajan@gmail.com>
2024-03-18 10:44:40 +0530

    fahrenheit to celcius

---
+++ ftoc.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main(){
+   
+    float F;
+    cout<<"enter the fahrenheit"<<endl;
+    cin>>F;
+    float c;
+    c=(F-32)*5/9;
+    cout<<"temperature in celcius is "<<c<<endl;
+    return 0;
+
+}
