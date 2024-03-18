7545e165c053b4085debfeebb0d03a182efc4a56
abhimahajan21 <abhi21mahajan@gmail.com>
2024-03-18 21:16:18 +0530

    Patterns

---
+++ pattern/22.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cin>>n;
+    int i=n;
+    while(i>=0){
+        int j=1;
+        while(j<=i){
+            cout<<"*";
+            j=j+1;;
+
+        }
+        cout<<endl;
+        i--;
+    }
+}
